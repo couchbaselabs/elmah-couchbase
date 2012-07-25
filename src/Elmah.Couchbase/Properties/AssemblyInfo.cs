cd4b89e50dbe87aaef03f86da710900a0a688268
jzablocki <dev@zblock.net>
2012-07-25 17:00:07 -0400

    Updated lib to include Couchbase 1.2 DP2

--- src/Elmah.Couchbase/Properties/AssemblyInfo.cs
+++ src/Elmah.Couchbase/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Elmah.Couchbase")]
+[assembly: AssemblyTitle("CouchbaseElmahErrorLog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Elmah.Couchbase")]
+[assembly: AssemblyProduct("CouchbaseElmahErrorLog")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1601fc5d-5c30-4993-ae96-6f7bcfc3221a")]
+[assembly: Guid("4202b9fa-28b5-4a1f-a7d2-bf51311d9471")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
